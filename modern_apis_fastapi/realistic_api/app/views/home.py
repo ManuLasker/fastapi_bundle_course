557e8f536b1c3dfdc875e8295eff1e5674f92b65
ManuLasker <emanuel.afanador@koombea.com>
2021-06-08 21:42:52 -0500

    feat: add view for home

---
+++ modern_apis_fastapi/realistic_api/app/views/home.py
@@ -0,0 +1,21 @@
+from app.models import report
+from fastapi.params import Depends
+from fastapi.responses import HTMLResponse, RedirectResponse
+from fastapi import APIRouter
+from fastapi import Request
+from app.app import templates
+from sqlalchemy.orm import Session
+from app.cache.deps import get_db
+from app.crud.crud_report import crud_report
+
+router = APIRouter()
+
+@router.get("/", response_class=HTMLResponse)
+def index(request: Request, db: Session = Depends(get_db)):
+    reports = crud_report.get_list_report(db)
+    return templates.TemplateResponse(name="home/index.html",
+                               context={"request": request, "events": reports})
+    
+@router.get("/favicon.ico")
+def favicon():
+    return RedirectResponse(url="/static/img/favicon.ico")
