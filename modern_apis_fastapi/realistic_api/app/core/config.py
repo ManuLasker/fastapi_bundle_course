f369e43443fb9597d8252d099fc743a8d4c78647
ManuLasker <emanuel.afanador@koombea.com>
2021-06-08 21:49:08 -0500

    feat: set up configurations from env variables

---
+++ modern_apis_fastapi/realistic_api/app/core/config.py
@@ -0,0 +1,15 @@
+from pydantic import BaseSettings
+from pydantic import AnyUrl
+import secrets
+
+class Settings(BaseSettings):
+    WEATHER_API_KEY: str
+    WEATHER_HOST: AnyUrl
+    STARTUP_EVENT_NAME: str = "startup"
+    SHUTDOWN_EVENT_NAME: str = "shutdown"
+    SECRET_KEY: str = secrets.token_urlsafe(32)
+    REDIS_HOST: str
+    CACHE_LIFETIME_IN_HOURS: float = 0.05
+    SQLALCHEMY_SQLITE_DATABASE_URI: str
+
+settings = Settings()
