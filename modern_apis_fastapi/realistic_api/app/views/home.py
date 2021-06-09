1763ac267baf583d26e7956598272c6223b09e5a
ManuLasker <emanuel.afanador@koombea.com>
2021-06-08 21:57:29 -0500

    fix: add some configurations for views endpoints

--- modern_apis_fastapi/realistic_api/app/views/home.py
+++ modern_apis_fastapi/realistic_api/app/views/home.py
@@ -10,12 +10,12 @@ from app.crud.crud_report import crud_report
 
 router = APIRouter()
 
-@router.get("/", response_class=HTMLResponse)
+@router.get("/", response_class=HTMLResponse, include_in_schema=False)
 def index(request: Request, db: Session = Depends(get_db)):
     reports = crud_report.get_list_report(db)
     return templates.TemplateResponse(name="home/index.html",
                                context={"request": request, "events": reports})
     
-@router.get("/favicon.ico")
+@router.get("/favicon.ico", include_in_schema=False)
 def favicon():
     return RedirectResponse(url="/static/img/favicon.ico")
